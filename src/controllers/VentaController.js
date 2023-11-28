85124db0bf4a458f37e645b2be7ee6dfc0b31dea
Juan Bohorquez Marcelo <juan.bohorquezm@perficient.com>
2023-11-27 20:43:36 -0500

    se empieza a trabajar la consulta de clientes

--- src/controllers/VentaController.js
+++ src/controllers/VentaController.js
@@ -2,8 +2,21 @@ function venta(req, res){
     res.render('venta/venta');
 }
 
-function consultarCliente(){
-
+function consultarCliente(req, res){
+    const data = req.body;
+    req.getConnection((err, conn) => {
+        
+        conn.query('SELECT * FROM cliente WHERE Identificacion = ?', [data.identificacion], (err, userdata) => {
+            if(userdata.length > 0) {
+                res.body = JSON.stringify(userdata)
+                userdata.forEach(element => {
+                    console.log(userdata);
+                });
+            }else {
+                res.render('venta/venta', {error: 'El cliente no existe!'})
+            }
+        });
+    });
 }
 
 function consultarCajero(){
